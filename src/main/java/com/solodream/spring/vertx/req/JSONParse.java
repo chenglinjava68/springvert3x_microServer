44d2e9cb15d3c4e7af4f18bbe445c231a26bcc01
Young <cywhoyi@gmail.com>
2015-11-27 11:36:50 +0800

    with fastjson framework's help,u can parse json request parameter.

--- src/main/java/com/solodream/spring/vertx/req/JSONParse.java
+++ src/main/java/com/solodream/spring/vertx/req/JSONParse.java
@@ -1,9 +1,12 @@
 package com.solodream.spring.vertx.req;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.TypeReference;
 import com.solodream.spring.vertx.req.client.DeviceRequestParam;
 import com.solodream.spring.vertx.req.client.UserLoginRequestParam;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by young on 15/11/25.
@@ -23,6 +26,36 @@ public class JSONParse {
         JsonReq<DeviceRequestParam> obj = JSON.parseObject(jsonString, new TypeReference<JsonReq<DeviceRequestParam>>() {
         });
 
+
+        String gps = "[\n" +
+                "  [\n" +
+                "    469913969,\n" +
+                "    20151127,\n" +
+                "    95543,\n" +
+                "    37385455,\n" +
+                "    0,\n" +
+                "    460143,\n" +
+                "    0,\n" +
+                "    14800,\n" +
+                "    788000\n" +
+                "  ],\n" +
+                "  [\n" +
+                "    525236411,\n" +
+                "    20151127,\n" +
+                "    95541,\n" +
+                "    37377919,\n" +
+                "    0,\n" +
+                "    482769,\n" +
+                "    0,\n" +
+                "    27300,\n" +
+                "    5213000\n" +
+                "  ]]";
+
+        JSONArray jsonArr = JSON.parseArray(gps);
+        for (Object array : jsonArr) {
+            JSONArray gpsArray = JSON.parseArray(String.valueOf(array));
+            System.out.println(gpsArray.get(0));
+        }
         System.out.println(obj.getParam().getImei());
     }
 }
