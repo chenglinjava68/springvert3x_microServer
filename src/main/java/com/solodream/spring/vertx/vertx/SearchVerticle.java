29acab80e5e0485e96273092b2be160023e9a12d
Young <cywhoyi@gmail.com>
2016-01-01 10:36:13 +0800

    support "HS256" Algorithm

---
+++ src/main/java/com/solodream/spring/vertx/vertx/SearchVerticle.java
@@ -0,0 +1,41 @@
+package com.solodream.spring.vertx.vertx;
+
+import com.solodream.spring.vertx.service.RouteService;
+import com.solodream.spring.vertx.service.SearchService;
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by young on 16/1/1.
+ */
+public class SearchVerticle extends AbstractVerticle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchVerticle.class);
+
+    @Autowired
+    private SearchService searchService;
+
+    public void start() {
+        vertx.eventBus().consumer("from", message -> {
+            LOGGER.info("Received a message: {}, {}", message.body(), message.headers());
+            try {
+//                JsonObject jsonString = (JsonObject) message.body();
+//                JsonReq<UserLoginRequestParam> obj = JSON.parseObject(jsonString.toString(), new TypeReference<JsonReq<UserLoginRequestParam>>() {
+//                });
+//                String username = obj.getParam().getUsername();
+//                String password = obj.getParam().getPassword();
+//                LOGGER.info("username is {},password is {}", username, password);
+//                ClientAccountInfoDto dto = clientService.login(username);
+//                if (dto != null && dto.getPassword().equals(password)) {
+//                    LOGGER.info("this {} login ", username);
+//                    message.reply(JSON.toJSONString(dto));
+//                } else {
+//                    message.fail(1201, "this username or password is wrong");
+//                }
+            } catch (Exception e) {
+                LOGGER.error("convert error.", e);
+            }
+        });
+    }
+}
