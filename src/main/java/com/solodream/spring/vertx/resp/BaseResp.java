0610680acf6abaf506bcdbf19b38c62147c809f6
Young <cywhoyi@gmail.com>
2016-01-07 12:00:09 +0800

    support jmx function

--- src/main/java/com/solodream/spring/vertx/resp/BaseResp.java
+++ src/main/java/com/solodream/spring/vertx/resp/BaseResp.java
@@ -1,25 +1,32 @@
 package com.solodream.spring.vertx.resp;
 
+import org.weakref.jmx.Managed;
+
 /**
  * Created by young on 15/12/31.
  */
 public class BaseResp<T extends BaseResp> {
+
     private int code = 100;
     private String msg;
     private T data;
 
+    @Managed
     public int getCode() {
         return code;
     }
 
+    @Managed
     public void setCode(int code) {
         this.code = code;
     }
 
+    @Managed
     public String getMsg() {
         return msg;
     }
 
+    @Managed
     public void setMsg(String msg) {
         this.msg = msg;
     }
