0672032682ad0e2c09221f4b0eb6b3842a611191
chenyang <cy@stlc.cn>
2016-10-14 20:06:46 +0800

    support  sms platform

---
+++ src/main/java/com/solodream/spring/vertx/vertx/ProfileVerticle.java
@@ -0,0 +1,34 @@
+package com.solodream.spring.vertx.vertx;
+
+import com.alibaba.fastjson.JSON;
+import com.solodream.spring.vertx.resp.BaseResp;
+import com.solodream.spring.vertx.resp.job.ProfileResponse;
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by young on 16/10/3.
+ */
+@Component
+public class ProfileVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProfileVerticle.class);
+
+    public void start() {
+        vertx.eventBus().consumer("profile", message -> {
+            LOGGER.info("Received a message: {}, {}", message.body(), message.headers());
+            try {
+                BaseResp<ProfileResponse> resp = new BaseResp<ProfileResponse>();
+                ProfileResponse response = new ProfileResponse();
+                response.setUserid(1);
+                resp.setData(response);
+                resp.setCode(0);
+                message.reply(JSON.toJSONString(resp));
+            } catch (Exception e) {
+                LOGGER.error("convert error.", e);
+            }
+        });
+    }
+}
